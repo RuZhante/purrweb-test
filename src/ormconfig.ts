0383c2e31a60a7f640c139ecf6967e80b76d88a5
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2021-09-19 15:02:14 +0600

    Added TypeORM, Connection options, Migrations

---
+++ src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'purrweb_test',
+  password: '123',
+  database: 'purrweb_test',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
+export default config;
